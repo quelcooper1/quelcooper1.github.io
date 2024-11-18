6dcbd1684e2cfd8e85195394ebae8b9f326a8444
quelcooper1 <marquel.cooper@walterlcohen.org>
2024-11-18 18:23:01 +0000

    <message about commit>

--- projects/platformer/setup.js
+++ projects/platformer/setup.js
@@ -1,9 +1,9 @@
 // setup variables
-const walkAcceleration = 4.5; // how much is added to the speed each frame
+const walkAcceleration = 4.8; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
 const friction = 1.5; // how much the player is slowed each frame
 const maxSpeed = 9; // maximum horizontal speed, not vertical
-const playerJumpStrength = 20; // this is subtracted from the speedY each jump
+const playerJumpStrength = 21; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 
 /////////////////////////////////////////////////
